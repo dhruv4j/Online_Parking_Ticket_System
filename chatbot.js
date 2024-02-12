4d8fb22763b8ddc3f815f195b5edd189cf26a26a
Om-Vilas-Kulkarni <kulkarniom-cmpn@atharvacoe.ac.in>
2024-02-13 01:44:48 +0530

    Updated Chatbot
    Updated Chatbot

--- chatbot.js
+++ chatbot.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = Digi_Park; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
